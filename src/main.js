586d48c18d930b44b68ad677b3126aa9083dc717
izzzorry <isabel.zorrilla@uao.edu.co>
2025-03-27 17:00:19 -0500

    bueno pues

--- src/main.js
+++ src/main.js
@@ -1,19 +1,6 @@
 // Importaciones necesarias
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC5RfGB1H6olHubSyO4ARAr-UPnFGCh8Kc",
-  authDomain: "videogames-366f5.firebaseapp.com",
-  projectId: "videogames-366f5",
-  storageBucket: "videogames-366f5.firebasestorage.app",
-  messagingSenderId: "1022961712480",
-  appId: "1:1022961712480:web:a05260cbf26fcc5db3b205"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db } from './config/firebase';
+import {collection, getDocs, addDoc, doc, updateDoc } from 'firebase/firestore';
 
 
 // Elementos del DOM
